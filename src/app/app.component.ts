251d9b70a1bea534c006d523607aef4e7175dc3d
Rafael Rodrigo <rafrodrigo@gmailom>
2024-03-08 15:51:56 -0300

    parte visual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { PagesModule } from './pages/pages.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+            RouterOutlet,
+            PagesModule,
+            HomeComponent
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
