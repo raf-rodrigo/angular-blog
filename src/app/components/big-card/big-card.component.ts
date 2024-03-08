64af3cb7c1f8643dfa7b487c147f3181d5d23156
Rafael Rodrigo <rafrodrigo@gmailom>
2024-03-08 16:29:22 -0300

    Add resposividade em Small and Big card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+  @Input() picture: string = "";
+  @Input() cardTitle: string = "";
+  @Input() cardContent: string = "";
+  @Input() cardReport: string = "";
 
 }
